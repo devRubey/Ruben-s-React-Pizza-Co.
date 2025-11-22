95fc969539e4f334833865c394e023fc6dd2ae56
devRubey <udohruben@gmail.com>
2025-11-22 17:08:00 +0100

    Deploy: build

--- src/index.js
+++ src/index.js
@@ -116,7 +116,10 @@ function Pizza({ pizzaObj }) {
 
   return (
     <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <img
+        src={process.env.PUBLIC_URL + "/" + pizzaObj.photoName}
+        alt={pizzaObj.name}
+      />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
